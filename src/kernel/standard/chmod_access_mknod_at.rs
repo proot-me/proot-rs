ef0a95d79430aedd2e3387a2729b9a28eace1efe
imlk <me@imlk.top>
2021-06-07 13:58:32 +0800

    feat: impl chmod_access_mknod_at::enter()

--- src/kernel/standard/chmod_access_mknod_at.rs
+++ src/kernel/standard/chmod_access_mknod_at.rs
@@ -1,14 +1,24 @@
-use crate::errors::Result;
+use std::os::unix::prelude::RawFd;
 
-pub fn enter() -> Result<()> {
+use crate::errors::*;
+use crate::filesystem::binding::Side;
+use crate::filesystem::Substitutor;
+use crate::process::tracee::Tracee;
+use crate::register::PtraceWriter;
+use crate::register::{Current, PtraceReader, SysArg, SysArg1, SysArg2};
+
+pub fn enter(tracee: &mut Tracee) -> Result<()> {
+    let dirfd = tracee.regs.get(Current, SysArg(SysArg1)) as RawFd;
+    let raw_path = tracee.regs.get_sysarg_path(SysArg2)?;
+
+    let canonical_guest_path = tracee.get_canonical_guest_path(dirfd, &raw_path, true)?;
+    let host_path = tracee.fs.substitute(&canonical_guest_path, Side::Guest)?;
+
+    tracee.regs.set_sysarg_path(
+        SysArg2,
+        &host_path,
+        "during enter open translation, setting host path",
+    )?;
+
     Ok(())
-
-    //                 dirfd = peek_reg(tracee, CURRENT, SYSARG_1);
-    //
-    //                 status = get_sysarg_path(tracee, path, SYSARG_2);
-    //                 if (status < 0)
-    //                    break;
-    //
-    //                 status = translate_path2(tracee, dirfd, path, SYSARG_2,
-    // REGULAR);
 }

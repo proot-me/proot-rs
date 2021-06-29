a78066ace756edbce292bedbf165b36dfba051a1
imlk <me@imlk.top>
2021-06-29 16:18:51 +0800

    test: add test for openat() and open()

--- src/kernel/standard/open.rs
+++ src/kernel/standard/open.rs
@@ -12,7 +12,6 @@ pub fn enter(tracee: &mut Tracee) -> Result<()> {
 
     let flags = OFlag::from_bits_truncate(tracee.regs.get(Current, SysArg(SysArg2)) as _);
 
-    debug!("open({:?}, {:?})", raw_path, flags);
     let deref_final = !(flags.contains(OFlag::O_NOFOLLOW)
         || (flags.contains(OFlag::O_EXCL) && flags.contains(OFlag::O_CREAT)));
     let host_path = tracee.fs.borrow().translate_path(raw_path, deref_final)?;
@@ -24,4 +23,57 @@ pub fn enter(tracee: &mut Tracee) -> Result<()> {
     )?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::tests::test_with_proot;
+
+    /// Unit test for the following syscalls:
+    /// - linkat
+    #[test]
+    fn test_open() {
+        test_with_proot(
+            |_tracee, _is_sysenter, _before_translation| {},
+            || {
+                let filepath = "/tmp/file_for_test_open";
+                let linkpath = "/tmp/link_for_test_open";
+
+                let result = std::panic::catch_unwind(|| {
+                    // init symlink
+                    std::os::unix::fs::symlink(filepath, linkpath).unwrap();
+
+                    // test open()
+
+                    // test open() with `O_CREAT` and `O_EXCL`
+                    // this will create a regular file at `filepath`
+                    let file_fd =
+                        nc::open(filepath, nc::O_RDONLY | nc::O_CREAT | nc::O_EXCL, 0o755).unwrap();
+                    let mut stat = nc::stat_t::default();
+                    nc::fstat(file_fd, &mut stat).unwrap();
+                    assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFREG);
+                    nc::close(file_fd).unwrap();
+
+                    // test open() with `OFlag::O_NOFOLLOW`;
+                    let file_fd = nc::open(linkpath, nc::O_NOFOLLOW | nc::O_PATH, 0).unwrap();
+                    let mut stat = nc::stat_t::default();
+                    nc::fstat(file_fd, &mut stat).unwrap();
+                    assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFLNK);
+                    nc::close(file_fd).unwrap();
+
+                    // test open() in normal case;
+                    let file_fd = nc::open(linkpath, 0, 0).unwrap();
+                    let mut stat = nc::stat_t::default();
+                    nc::fstat(file_fd, &mut stat).unwrap();
+                    assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFREG);
+                    nc::close(file_fd).unwrap();
+                });
+                std::fs::remove_file(linkpath).unwrap();
+                std::fs::remove_file(filepath).unwrap();
+                if let Err(err) = result {
+                    std::panic::resume_unwind(err);
+                }
+            },
+        )
+    }
 }

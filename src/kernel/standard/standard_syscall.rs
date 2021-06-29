dcfc5efebfa185874c7f8895aaec8c7ad3c97641
imlk <me@imlk.top>
2021-06-29 16:57:50 +0800

    test: add test for standard_syscall

--- src/kernel/standard/standard_syscall.rs
+++ src/kernel/standard/standard_syscall.rs
@@ -18,3 +18,49 @@ pub fn enter(tracee: &mut Tracee) -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+
+    use crate::utils::tests::test_with_proot;
+
+    /// Unit test for all the standard syscalls:
+    /// access, acct, chmod, chown, chroot, getxattr, listxattr, mknod, creat,
+    /// removexattr, setxattr, stat, swapoff, swapon, truncate, umount2, uselib,
+    /// utime, utimes
+    ///
+    /// Since the arguments of those syscalls follow a certain pattern, only the
+    /// stat() call is tested in our unit tests.
+    #[test]
+    fn test_standard_syscall() {
+        test_with_proot(
+            |_tracee, _is_sysenter, _before_translation| {},
+            || {
+                let filepath = "/tmp/file_for_test_standard_syscall";
+                let linkpath = "/tmp/link_for_test_standard_syscall";
+
+                let result = std::panic::catch_unwind(|| {
+                    // init file and symlink file
+                    File::create(filepath).unwrap();
+                    std::os::unix::fs::symlink(filepath, linkpath).unwrap();
+
+                    // test stat()
+
+                    let mut stat = nc::stat_t::default();
+                    nc::stat(linkpath, &mut stat).unwrap();
+                    // should be a regular file, since symbol link file will be dereference
+                    // automatically.
+                    assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFREG);
+                    nc::stat(filepath, &mut stat).unwrap();
+                    assert_eq!((stat.st_mode & nc::S_IFMT), nc::S_IFREG);
+                });
+                std::fs::remove_file(filepath).unwrap();
+                std::fs::remove_file(linkpath).unwrap();
+                if let Err(err) = result {
+                    std::panic::resume_unwind(err);
+                }
+            },
+        )
+    }
+}

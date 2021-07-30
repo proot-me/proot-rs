0fcc25a74a1d40ab55135a7b0e2091892924f66e
imlk <me@imlk.top>
2021-07-30 12:32:15 +0800

    refactor: porting missing parts of files from proot

--- src/kernel/execve/loader/assembly-arm64.h
+++ src/kernel/execve/loader/assembly-arm64.h
@@ -20,12 +20,12 @@
  * 02110-1301 USA.
  */
 
-/* According to the ARM EABI, all registers have undefined values at
+/* According to the ARM64 EABI, all registers have undefined values at
  * program startup except:
  *
- * - the instruction pointer (r15)
- * - the stack pointer (r13)
- * - the rtld_fini pointer (r0)
+ * - the instruction pointer (pc)
+ * - the stack pointer (sp)
+ * - the rtld_fini pointer (x0)
  */
 #define BRANCH(stack_pointer, destination) do {			\
 	asm volatile (						\
@@ -33,29 +33,32 @@
 		"mov sp, %0				\n\t"	\
 		"					\n\t"	\
 		"// Clear rtld_fini.			\n\t"	\
-		"mov r0, #0				\n\t"	\
+		"mov x0, #0				\n\t"	\
 		"					\n\t"	\
 		"// Start the program.			\n\t"	\
-		"mov pc, %1				\n"	\
+		"br %1				\n"		\
 		: /* no output */				\
 		: "r" (stack_pointer), "r" (destination)	\
-		: "memory", "sp", "r0", "pc");			\
+		: "memory", "sp", "x0");			\
 	__builtin_unreachable();				\
 	} while (0)
 
 #define PREPARE_ARGS_1(arg1_)				\
-	register word_t arg1 asm("r0") = arg1_;		\
+	register word_t arg1 asm("x0") = arg1_;		\
 
 #define PREPARE_ARGS_3(arg1_, arg2_, arg3_)		\
 	PREPARE_ARGS_1(arg1_)				\
-	register word_t arg2 asm("r1") = arg2_;		\
-	register word_t arg3 asm("r2") = arg3_;		\
+	register word_t arg2 asm("x1") = arg2_;		\
+	register word_t arg3 asm("x2") = arg3_;		\
+
+#define PREPARE_ARGS_4(arg1_, arg2_, arg3_, arg4_)	\
+	PREPARE_ARGS_3(arg1_, arg2_, arg3_)		\
+	register word_t arg4 asm("x3") = arg4_;		\
 
 #define PREPARE_ARGS_6(arg1_, arg2_, arg3_, arg4_, arg5_, arg6_)	\
-	PREPARE_ARGS_3(arg1_, arg2_, arg3_)				\
-	register word_t arg4 asm("r3") = arg4_;				\
-	register word_t arg5 asm("r4") = arg5_;				\
-	register word_t arg6 asm("r5") = arg6_;
+	PREPARE_ARGS_4(arg1_, arg2_, arg3_, arg4_)			\
+	register word_t arg5 asm("x4") = arg5_;				\
+	register word_t arg6 asm("x5") = arg6_;
 
 #define OUTPUT_CONTRAINTS_1			\
 	"r" (arg1)
@@ -64,30 +67,32 @@
 	OUTPUT_CONTRAINTS_1,			\
 	"r" (arg2), "r" (arg3)
 
-#define OUTPUT_CONTRAINTS_6				\
-	OUTPUT_CONTRAINTS_3,				\
-	"r" (arg4), "r" (arg5), "r" (arg6)
+#define OUTPUT_CONTRAINTS_4			\
+	OUTPUT_CONTRAINTS_3,			\
+	"r" (arg4)
 
-#define SYSCALL(number_, nb_args, args...)			\
-	({							\
-		register word_t number asm("r7") = number_;	\
-		register word_t result asm("r0");		\
-		PREPARE_ARGS_##nb_args(args)			\
-			asm volatile (				\
-				"svc #0x00000000	\n\t"	\
-				: "=r" (result)			\
-				: "r" (number),			\
-				OUTPUT_CONTRAINTS_##nb_args	\
-				: "memory");			\
-			result;					\
+#define OUTPUT_CONTRAINTS_6				\
+	OUTPUT_CONTRAINTS_4,				\
+	"r" (arg5), "r" (arg6)
+
+#define SYSCALL(number_, nb_args, args...)				\
+	({								\
+		register word_t number asm("x8") = number_;		\
+		register word_t result asm("x0");			\
+		PREPARE_ARGS_##nb_args(args)				\
+			asm volatile (					\
+				"svc #0x00000000		\n\t"	\
+				: "=r" (result)				\
+				: "r" (number),				\
+				OUTPUT_CONTRAINTS_##nb_args		\
+				: "memory");				\
+			result;						\
 	})
 
-#define OPEN	5
-#define CLOSE	6
-#define MMAP	192
-#define MMAP_OFFSET_SHIFT 12
-#define EXECVE	11
-#define EXIT	1
-#define PRCTL	172
-#define MPROTECT 125
-
+#define OPENAT	56
+#define CLOSE	57
+#define MMAP	222
+#define EXECVE	221
+#define EXIT	93
+#define PRCTL   167
+#define MPROTECT 226
